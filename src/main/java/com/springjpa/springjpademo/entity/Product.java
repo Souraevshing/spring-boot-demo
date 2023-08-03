9961fed89172a075e4463ee166338ba0e0597ec5
Sourav Kumar <isauravshing@gmail.com>
2023-08-03 18:11:54 +0530

    add lombok library to reduce boilerplate code

--- src/main/java/com/springjpa/springjpademo/entity/Product.java
+++ src/main/java/com/springjpa/springjpademo/entity/Product.java
@@ -1,6 +1,10 @@
 package com.springjpa.springjpademo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,6 +16,10 @@ import java.time.LocalDateTime;
 //columnNames = which column name to be changed
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table( name = "products",
         schema = "ecommerce",
         uniqueConstraints = {
@@ -65,91 +73,4 @@ public class Product {
 
     @UpdateTimestamp
     private LocalDateTime dateUpdated;
-
-    public Product() {
-
-    }
-    public Product(long id, String sku, String name, String description, BigDecimal price, boolean active, String imageUrl, LocalDateTime dateCreated, LocalDateTime dateUpdated) {
-        this.id = id;
-        this.sku = sku;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.active = active;
-        this.imageUrl = imageUrl;
-        this.dateCreated = dateCreated;
-        this.dateUpdated = dateUpdated;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public LocalDateTime getDateUpdated() {
-        return dateUpdated;
-    }
-
-    public void setDateUpdated(LocalDateTime dateUpdated) {
-        this.dateUpdated = dateUpdated;
-    }
 }
