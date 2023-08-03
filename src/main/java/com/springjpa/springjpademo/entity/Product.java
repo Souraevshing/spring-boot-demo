d3a2bbff436acf7742ea5816d5edad1ab7e28d1d
Sourav Kumar <isauravshing@gmail.com>
2023-08-03 16:51:12 +0530

    types of sequence in hibernate

--- src/main/java/com/springjpa/springjpademo/entity/Product.java
+++ src/main/java/com/springjpa/springjpademo/entity/Product.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+//UniqueConstraint is used to specify any column to be always unique
+//name = unique name
+//columnNames = which column name to be changed
+
 @Entity
 @Table( name = "products",
         schema = "ecommerce",
@@ -17,8 +21,29 @@ import java.time.LocalDateTime;
 )
 public class Product {
 
+//GeneratedValue is used to specify primary key generation type
+//here we have used IDENTITY that relies on db auto-increment column
+
+//    Hibernate provide 4 types of GenerationType :
+//    1. GenerationType.AUTO : this is the default generation type and let the persistence provider choose the generation strategy
+//    2. Generation.IDENTITY : relies on auto-incremented db column and lets db generate a new value with each insert operation
+//    3. Generation.SEQUENCE : used to generate primary key values and used db sequence to generate unique values, most commonly used.
+//                             Extra select statement is used and SequenceGenerator annotation is used to provide name, sequence name, allocationSize.
+//                             By default, allocationSize is 50. Whenever any rows are added, hibernate will search and select query from the sequence name.
+//    4. Generation.TABLE : used rarely, generates sequence by storing & updating its current value in db. It requires the use of pessimistic locks which
+//                             put all transactions into order which makes it very slow, so we don't use this nowadays.
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_generator"
+    )
+
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
     private long id;
     @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
