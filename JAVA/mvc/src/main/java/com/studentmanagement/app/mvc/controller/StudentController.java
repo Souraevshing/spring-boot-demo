4214cb5dd65e052f45eafb642420717c0f659ba4
Sourav Kumar <isauravshing@gmail.com>
2023-08-21 18:55:53 +0530

    add navbar and minor changes to the ui

--- JAVA/mvc/src/main/java/com/studentmanagement/app/mvc/controller/StudentController.java
+++ JAVA/mvc/src/main/java/com/studentmanagement/app/mvc/controller/StudentController.java
@@ -4,9 +4,7 @@ import com.studentmanagement.app.mvc.entity.Student;
 import com.studentmanagement.app.mvc.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -37,6 +35,12 @@ public class StudentController {
     public String saveStudent(@ModelAttribute ("student") Student student) {
         studentService.saveStudent(student);
         return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit{id}")
+    public String editStudent(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit";
     }
 
 }
