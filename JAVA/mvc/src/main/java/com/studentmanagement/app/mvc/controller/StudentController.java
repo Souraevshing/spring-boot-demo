59b517992f9dd957fc338a34bab9e9d715db6e98
Sourav Kumar <isauravshing@gmail.com>
2023-08-21 17:48:19 +0530

    createStudent complete

--- JAVA/mvc/src/main/java/com/studentmanagement/app/mvc/controller/StudentController.java
+++ JAVA/mvc/src/main/java/com/studentmanagement/app/mvc/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.studentmanagement.app.mvc.controller;
 
+import com.studentmanagement.app.mvc.entity.Student;
 import com.studentmanagement.app.mvc.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -17,9 +20,23 @@ public class StudentController {
     }
 
     @GetMapping("/students")
-    public String listStudents(Model model) {
+    public String getAllStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/students/create")
+    public String createStudent(Model model) {
+        //creating student obj to hold form data.
+        Student student = new Student();
+        model.addAttribute("student",student);
+        return "create";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute ("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 
 }
