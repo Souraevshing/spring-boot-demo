809dacbfa3e480116d30bfd6a78e3fd7f7d9ed57
Sourav Kumar <isauravshing@gmail.com>
2023-08-27 09:39:51 +0530

    add custom information for swagger-ui

--- src/main/java/springrestapii/demo/RestapiApplication.java
+++ src/main/java/springrestapii/demo/RestapiApplication.java
@@ -1,5 +1,10 @@
 package springrestapii.demo;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +13,25 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:env.properties")
+@OpenAPIDefinition(info = @Info(
+		title = "Spring Boot RestAPI.",
+		description = "Developed using Spring Mvc, Jpa, Mysql connector and 3rd party libraries such as Lombok, MapStruct, Spring-validation, SpringBoot actuator & SpringDoc openAPI.",
+		version = "1.0.0",
+		contact = @Contact(
+				name = "Sourav Kumar",
+				email = "isauravshing@gmail.com",
+				url = "https://sauraevshing-portfolio.netlify.app/"
+		),
+		license = @License(
+				name = "Apache 2.0",
+				url = "https://sauraevshing-portfolio.netlify.app/"
+		)
+),
+		externalDocs = @ExternalDocumentation(
+				description = "RestAPI Documentation",
+				url = "https://sauraevshing-portfolio.netlify.app/"
+		)
+)
 public class RestapiApplication {
 
 	@Bean
